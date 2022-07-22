c153a0b3ae0eeecf4ffc926a6b8844d434947b1b
Daniel Dirdala <Daniel.dirdala1@gmail.com>
2022-07-21 17:45:31 -0700

    Update user_interface_functions.cpp
    Added more cases for configuration

--- Gizmo/src/user_interface/user_interface_functions.cpp
+++ Gizmo/src/user_interface/user_interface_functions.cpp
@@ -16,7 +16,24 @@ enum ui_state_enum {INACTIVE,
                         SCORE_BACK,
                     MENU_CAL,
                         CAL_BACK,
-                        CAL_START};
+                        CAL_START,
+                    CON_BACK,       //CALIBRATION DISPLAY STATES
+                        CON_WALK,
+                            WALK_BACK,
+                            WALK_3,
+                            WALK_6,
+                            WALK_9,
+                        CON_TIME,
+                            TIME_BACK,
+                            TIME_90,
+                            TIME_180,
+                            TIME_240,
+                        CON_STOP,
+                            STOP_BACK,
+                            STOP_5,
+                            STOP_10,
+                            STOP_20,
+                        };
 
 // Variables
 uint8_t ui_state = INACTIVE;
@@ -151,15 +168,207 @@ String user_interface_class::ui_menu()
                 }
                 break;
 
-            case CAL_START:
+            case CAL_START: //select start calibration = config menu (back selected)
                 if(ui_button.checkSelect()){
-                    ui_state = INACTIVE;
-                    selection = "CALIBRATION";
+                    ui_state = CON_BACK;
+                    oled.config_back();
+                }
+
+                else if(ui_button.checkNext()) {//next selection back in cal start menu 
+                    ui_state = CAL_BACK;
+                    oled.cal_back();
+                }
+                break;
+
+            case CON_BACK: //Config Back (back is selected with walk, stop, time selections)
+                if(ui_button.checkSelect()) {//goes back to previous screen
+                    ui_state = CAL_START;
+                    oled.cal_start();
                 }
 
                 else if(ui_button.checkNext()) {
-                    ui_state = CAL_BACK;
-                    oled.cal_back();
+                      ui_state = CON_WALK;
+                      oled.config_walk();
+                }
+                break;
+
+                case CON_WALK:
+                if(ui_button.checkSelect()) {
+                    ui_state = WALK_BACK;
+                    oled.walk_back();
+                }
+
+                else if(ui_button.checkNext()) {
+                      ui_state = CON_STOP;
+                      oled.config_stop();
+                }
+                break;
+
+                case WALK_BACK:
+                if(ui_button.checkSelect()) {
+                    ui_state = CON_WALK;
+                    oled.config_walk();
+                }
+
+                else if(ui_button.checkNext()) {
+                      ui_state = WALK_3;
+                      oled.walk_3();
+                }
+                break;
+
+                case WALK_3:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;//set as inactive return to MENU state
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = WALK_6;
+                    oled.walk_6();
+                }
+                break;
+
+                case WALK_6:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = WALK_9;
+                    oled.walk_9();
+                }
+                break;
+
+                case WALK_9:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = WALK_BACK;
+                    oled.walk_back();
+                }
+                break;
+
+                case CON_STOP:
+                if(ui_button.checkSelect()) {
+                    ui_state = STOP_BACK;
+                    oled.stop_back();
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = CON_TIME;
+                    oled.config_time();
+                }
+                break;
+
+                case STOP_BACK:
+                if(ui_button.checkSelect()) {
+                    ui_state = CON_STOP;
+                    oled.config_stop();
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = STOP_5;
+                    oled.stop_5();
+                }
+                break;
+
+                case STOP_5:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = STOP_10;
+                    oled.stop_10();
+                }
+                break;
+
+                case STOP_10:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = STOP_20;
+                    oled.stop_20();
+                }
+                break;
+
+                case STOP_20:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = STOP_BACK;
+                    oled.stop_back();
+                }
+                break;
+
+                case CON_TIME:
+                if(ui_button.checkSelect()) {
+                    ui_state = TIME_BACK;
+                    oled.time_back();
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = CON_BACK;
+                    oled.config_back();
+                }
+                break;
+
+                case TIME_BACK:
+                if(ui_button.checkSelect()) {
+                    ui_state = CON_TIME;
+                    oled.config_time();
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = TIME_90;
+                    oled.time_90();
+                }
+                break;
+
+                case TIME_90:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = TIME_180;
+                    oled.time_180();
+                }
+                break;
+
+                case TIME_180:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = TIME_240;
+                    oled.time_240();
+                }
+                break;
+
+                case TIME_240:
+                if(ui_button.checkSelect()) {
+                    ui_state = INACTIVE;
+                    selection = "NULL";
+                }
+
+                else if(ui_button.checkNext()) {
+                    ui_state = TIME_BACK;
+                    oled.time_back();
                 }
                 break;
         }
