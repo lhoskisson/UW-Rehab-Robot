2410967252885f2c3b60ed18d40f4c97259b09aa
lhoskisson <72849914+lhoskisson@users.noreply.github.com>
2022-10-18 16:20:17 -0700

    attempting to fix button state issues

--- Gizmo/src/user_interface/src/ui_button/ui_button_functions.cpp
+++ Gizmo/src/user_interface/src/ui_button/ui_button_functions.cpp
@@ -24,8 +24,13 @@ void ui_button_class::registerPress()
   unsigned long currentMillis = millis();
   if(waitingForRelease)
   {
-    if(clickTimes[1] == 0 && clickTimes[0] == 0 && currentMillis - clickTimes[1] > BUTTON_DEBOUNCE) //catch second click release
+//    Serial.print("Waiting for Release ");
+//    Serial.print(clickTimes[0]);
+//    Serial.print(" ");
+//    Serial.println(clickTimes[1]);
+    if(clickTimes[1] != 0 && clickTimes[0] != 0 && currentMillis - clickTimes[1] > BUTTON_DEBOUNCE) //catch second click release
     {
+      //Serial.println("in second release");
       releaseTime = currentMillis;
       waitingForRelease = false;
       setButtonStatus();
@@ -33,6 +38,7 @@ void ui_button_class::registerPress()
     }
     else if(clickTimes[1] == 0 && clickTimes[0] != 0 && (currentMillis - clickTimes[0] > BUTTON_DEBOUNCE)) //catch first click release
     {
+      //Serial.println("in first release");
       releaseTime = currentMillis;
       waitingForRelease = false;
       setButtonStatus();
@@ -49,11 +55,15 @@ void ui_button_class::registerPress()
   {
     clickTimes[0] = currentMillis;
     waitingForRelease = true;
+//    Serial.print("Setting clickTimes[0]: ");
+//    Serial.println(clickTimes[0]);
   }
   else if(clickTimes[1] == 0 && clickTimes[0] != 0 && currentMillis - clickTimes[0] > BUTTON_DEBOUNCE)
   {
     clickTimes[1] = currentMillis;
     waitingForRelease = true;
+//    Serial.print("Setting clickTimes[1]: ");
+//    Serial.println(clickTimes[1]);
   }
 }
 
@@ -63,6 +73,7 @@ bool ui_button_class::checkSelect()
     return false;
   if(buttonQueue.getHead() == SELECT)
   {
+    //Serial.println("dequeuing SELECT");
     buttonQueue.dequeue();
     return true;
   }
@@ -75,6 +86,7 @@ bool ui_button_class::checkNext()
     return false;
   if(buttonQueue.getHead() == NEXT)
   {
+    //Serial.println("dequeuing NEXT");
     buttonQueue.dequeue();
     return true;
   }
@@ -83,40 +95,49 @@ bool ui_button_class::checkNext()
 
 void ui_button_class::setButtonStatus()
 {
-  interrupts();
+  //Serial.println("In setButtonStatus");
   int queueSize = buttonQueue.itemCount();
+
+  //TODO:this loop is not the best way to go about this as it stalls execution, maybe implement with timers in future.
+  //This code is really scuffed right now, for a specific button input the code will break and go into a state
+  //where waitingForRelease is set to the incorrect value and the code thinks that a button press is a release.
+  //This is hard to debug because the Serial print statements affect the timing of the code and so the existince 
+  //and/or timing of the issue varies between when the code that has the Serial print statments and when the code does not.
   while(true)
-  {
+  { 
     if(clickTimes[0] == 0 && clickTimes[1] == 0)
       break;
-      
-    if((millis() - clickTimes[0]) > SELECT_DELTA)
+    noInterrupts();
+    if((millis() - clickTimes[0]) > SELECT_DELTA && clickTimes[1] == 0 && clickTimes[0] != 0 && !waitingForRelease)
     {
       Serial.println("NEXT");
       Serial.println(clickTimes[0]);
       Serial.println(clickTimes[1]);
+      
       //1 click => NEXT
-      noInterrupts();
+      //noInterrupts();
       buttonQueue.enqueue(NEXT);
       clickTimes[0] = 0;
       clickTimes[1] = 0;
-      interrupts();
+      waitingForRelease = false;
+      //interrupts();
       break;
     }
-    else if(clickTimes[1] > 0 && (clickTimes[1] - clickTimes[0]) < SELECT_DELTA)
+    else if(clickTimes[1] > 0 && clickTimes[0] > 0 && (clickTimes[1] - clickTimes[0]) < SELECT_DELTA && !waitingForRelease)
     {
       Serial.println("SELECT");
       Serial.println(clickTimes[0]);
       Serial.println(clickTimes[1]);
       
-      //2 clicks => SELECT
-      noInterrupts();
+      //2 clicks => SELECT     
       buttonQueue.enqueue(SELECT);
       clickTimes[0] = 0;
       clickTimes[1] = 0;
-      interrupts();
+      waitingForRelease = false;
+      //interrupts();
       break;
     }
+    interrupts();
   }
 }
 
